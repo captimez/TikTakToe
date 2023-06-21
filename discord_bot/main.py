3b462a6f66dbb7361393c8963835f0c918a2325e
Tim <tim.kl@hotmail.de>
2023-06-21 18:18:32 +0200

    small changes

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,5 +1,4 @@
-#URL: https://discord.com/api/oauth2/authorize?client_id=1103813311809720431&permissions=1634235578432&scope=bot
-#TOKEN: MTEwMzgxMzMxMTgwOTcyMDQzMQ.GSKBAq.OYz7LlA1U0FrkvRGT_6lmnkZwoInwyWGbn_T0Q
+
 
 import bot
 
